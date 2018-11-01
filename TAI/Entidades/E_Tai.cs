9697b04c8fc29d0c38d8ae724a443797bca526f3
ArceS <sarce23@uabc.edu.mx>
2018-10-31 21:59:11 -0700

    base de datos, negocios y entidades

---
+++ TAI/Entidades/E_Tai.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class E_Usuario
+    {
+        #region Atributos
+        private string _Accion;
+        private int _IdUsuarios;
+        private string _NombreUsuario;
+        private string _EmailUsuario;
+        private string _PasswordUsuario;
+        private string _EstatusUsuario;
+
+        private string _NombreNegocio;
+        private string _DireccionNegocio;
+        #endregion
+
+        #region Contructor
+        public E_Usuario()
+        {
+            _Accion = string.Empty;
+            _IdUsuarios = 0;
+            _NombreUsuario = string.Empty;
+            _EmailUsuario = string.Empty;
+            _PasswordUsuario = string.Empty;
+            _EstatusUsuario = string.Empty;
+            _NombreNegocio = string.Empty;
+            _DireccionNegocio = string.Empty;
+        }
+        #endregion
+
+        #region Encapsulamiento
+        public string Accion { get => _Accion; set => _Accion = value; }
+        public int IdUsuarios { get => _IdUsuarios; set => _IdUsuarios = value; }
+        public string NombreUsuario { get => _NombreUsuario; set => _NombreUsuario = value; }
+        public string EmailUsuario { get => _EmailUsuario; set => _EmailUsuario = value; }
+        public string PasswordUsuario { get => _PasswordUsuario; set => _PasswordUsuario = value; }
+        public string EstatusUsuario { get => _EstatusUsuario; set => _EstatusUsuario = value; }
+        public string NombreNegocio { get => _NombreNegocio; set => _NombreNegocio = value; }
+        public string DireccionNegocio { get => _DireccionNegocio; set => _DireccionNegocio = value; }
+        #endregion
+
+    }
+
+    public class E_Rol
+    {
+        #region Atributos
+        private string _Accion;
+        private int _IdRol;
+        private string _NombreRol;
+        private string _Descripcion;
+        #endregion
+
+        #region Contructor
+        public E_Rol()
+        {
+            _Accion = string.Empty;
+            _IdRol = 0;
+            _NombreRol = string.Empty;
+            _Descripcion = string.Empty;
+        }
+        #endregion
+
+        #region Encapsulamiento
+        public string Accion { get => _Accion; set => _Accion = value; }
+        public int IdRol { get => _IdRol; set => _IdRol = value; }
+        public string NombreRol { get => _NombreRol; set => _NombreRol = value; }
+        public string Descripcion { get => _Descripcion; set => _Descripcion = value; }
+        #endregion
+    }
+
+    public class E_Producto
+    {
+        #region Atributos
+        private string _Accion;
+        private int _IdProducto;
+        private string _NombreProducto;
+        private string _Marca;
+        private string _Descripcion;
+        private int _CostoProducto;
+        private int _CantidadProducto;
+        #endregion
+
+        #region Constructor
+        public E_Producto()
+        {
+            _Accion = string.Empty;
+            _IdProducto = 0;
+            _NombreProducto = string.Empty;
+            _Marca = string.Empty;
+            _Descripcion = string.Empty;
+            _CostoProducto = 0;
+            _CantidadProducto = 0;
+        }
+        #endregion
+
+        #region Encapsulamiento
+        public string Accion { get => _Accion; set => _Accion = value; }
+        public int IdProducto { get => _IdProducto; set => _IdProducto = value; }
+        public string NombreProducto { get => _NombreProducto; set => _NombreProducto = value; }
+        public string Marca { get => _Marca; set => _Marca = value; }
+        public string Descripcion { get => _Descripcion; set => _Descripcion = value; }
+        public int CostoProducto { get => _CostoProducto; set => _CostoProducto = value; }
+        public int CantidadProducto { get => _CantidadProducto; set => _CantidadProducto = value; }
+        #endregion
+    }
+
+    public class E_UsuarioRol
+    {
+        #region Atributos
+        private string _Accion;
+        private int _IdUsuario;
+        private int _IdRol;
+        #endregion
+
+        #region Contructor
+        public E_UsuarioRol()
+        {
+            _IdRol = 0;
+            _IdUsuario = 0;
+            _Accion = string.Empty;
+        }
+        #endregion
+
+        #region Encapsulamiento
+        public string Accion { get => _Accion; set => _Accion = value; }
+        public int IdUsuario { get => _IdUsuario; set => _IdUsuario = value; }
+        public int IdRol { get => _IdRol; set => _IdRol = value; }
+        #endregion
+    }
+
+    public class E_Negocios
+    {
+        #region Atributos
+        private string _Accion;
+        private int _IdNegocios;
+        private string _DiasLaborales;
+        private string _TipoNegocio;
+        private string _TelefonoNegocio;
+        private string _EstatusUsuario;
+        private string _NombreNegocio;
+        private string _DireccionNegocio;
+        #endregion
+
+        #region Contructor
+        public E_Negocios()
+        {
+            _Accion = string.Empty;
+            _IdNegocios = 0;
+            _DiasLaborales = string.Empty;
+            _TipoNegocio = string.Empty;
+            _TelefonoNegocio = string.Empty;
+            _EstatusUsuario = string.Empty;
+            _NombreNegocio = string.Empty;
+            _DireccionNegocio = string.Empty;
+        }
+        #endregion
+
+        #region Encapsulamiento
+        public string Accion { get => _Accion; set => _Accion = value; }
+        public int IdNegocio { get => _IdNegocios; set => _IdNegocios = value; }
+        public string EDiasLaborales { get => _DiasLaborales; set => _DiasLaborales = value; }
+        public string TipoNegocio { get => _TipoNegocio; set => _TipoNegocio = value; }
+        public string TelefonoNegocio { get => _TelefonoNegocio; set => _TelefonoNegocio = value; }
+        public string EstatusUsuario { get => _EstatusUsuario; set => _EstatusUsuario = value; }
+        public string NombreNegocio { get => _NombreNegocio; set => _NombreNegocio = value; }
+        public string DireccionNegocio { get => _DireccionNegocio; set => _DireccionNegocio = value; }
+        #endregion
+
+        public class E_UsuarioNegocios
+        {
+            #region Atributos
+            private string _Accion;
+            private int _IdUsuario;
+            private int _IdNegocios;
+            #endregion
+
+            #region Contructor
+            public E_UsuarioNegocios()
+            {
+                _IdNegocios = 0;
+                _IdUsuario = 0;
+                _Accion = string.Empty;
+            }
+            #endregion
+
+            #region Encapsulamiento
+            public string Accion { get => _Accion; set => _Accion = value; }
+            public int IdUsuario { get => _IdUsuario; set => _IdUsuario = value; }
+            public int IdNegocios { get => _IdNegocios; set => _IdNegocios = value; }
+            #endregion
+        }
+    }
+}
---
+++ TAI/Negocios/N_Tai.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatosSQL;
+using Entidades;
+using System.Data;
+using System.Net;
+using System.Net.Mail;
+
+namespace Negocios
+{
+    public class N_Usuario
+    {
+        D_IBM_Datos ObjIBM = new D_IBM_Datos();
+        D_Listados ObjLst = new D_Listados();
+        private string Sp = "IBM_Usuario";
+
+        //Acciones de insertar, Borrar y Modificar los datos del usuario
+        public string InsertarUsuario(E_Usuario pEntidad)
+        {
+            pEntidad.Accion = "INSERTAR";
+            return ObjIBM.IBM_Entidad<E_Usuario>(Sp, pEntidad);
+        }
+
+        public string BorraUsuario(int pIdUsuarios)
+        {
+            E_Usuario Entidad = new E_Usuario
+            {
+                Accion = "BORRAR",
+                IdUsuarios = pIdUsuarios
+            };
+            return ObjIBM.IBM_Entidad<E_Usuario>(Sp, Entidad);
+        }
+
+        public string ModoficaUsuario(E_Usuario pEntidad)
+        {
+            pEntidad.Accion = "MODIFICAR";
+            return ObjIBM.IBM_Entidad<E_Usuario>(Sp, pEntidad);
+        }
+
+        //listado general de usuario en formato DataTable y list <E_Sistema>
+        public DataTable GetDT_Usuarios()
+        {
+            return ObjLst.DT_ListadoGeneral("[tbUsuarios]", "NombreUsuario");
+        }
+
+        public List<E_Usuario> LstUsuarios()
+        {
+            return D_ConvierteDatos.ConvertirDTALista<E_Usuario>(GetDT_Usuarios());
+        }
+
+        // Busqueda de usuarios por diferente creiterio
+
+        public E_Usuario BuscarUsuarioPorId(int pIdUsuario)
+        {
+            return (from User in LstUsuarios() where User.IdUsuarios == pIdUsuario select User).FirstOrDefault();
+        }
+
+        public E_Usuario BuscarUsuarioPorEmail(string pEmail)
+        {
+            return (from User in LstUsuarios() where User.EmailUsuario == pEmail select User).FirstOrDefault();
+        }
+
+        public E_Usuario BuscarUsuarioPorNombre(string pNombre)
+        {
+            return (from User in LstUsuarios() where User.NombreUsuario == pNombre select User).FirstOrDefault();
+        }
+
+        public E_Usuario ValidaLogin(string Email, string Password)
+        {
+            return (from User in LstUsuarios() where User.EmailUsuario == Email && User.PasswordUsuario == Password select User).FirstOrDefault();
+        }
+
+        public E_Usuario BuscarUsuarioPorNombreNegocio(string NombreNegocio, int IdUsuario)
+        {
+            return (from User in LstUsuarios() where User.NombreNegocio == NombreNegocio && User.IdUsuarios == IdUsuario select User).FirstOrDefault();
+        }
+
+    }
+
+    public class N_Rol
+    {
+        D_IBM_Datos ObjIBM = new D_IBM_Datos();
+        D_Listados ObjLst = new D_Listados();
+        private string Sp = "IBM_Rol";
+
+        //Acciones de insertar, Borrar y Modificar los datos del Rol
+        public string InsertarRol(E_Rol pEntidad)
+        {
+            pEntidad.Accion = "INSERTAR";
+            return ObjIBM.IBM_Entidad<E_Rol>(Sp, pEntidad);
+        }
+
+        public string BorraRol(int pIdRol)
+        {
+            E_Rol Entidad = new E_Rol
+            {
+                Accion = "BORRAR",
+                IdRol = pIdRol
+            };
+            return ObjIBM.IBM_Entidad<E_Rol>(Sp, Entidad);
+        }
+
+        public string ModoficaRol(E_Rol pEntidad)
+        {
+            pEntidad.Accion = "MODIFICAR";
+            return ObjIBM.IBM_Entidad<E_Rol>(Sp, pEntidad);
+        }
+
+        //listado general de Rol en formato DataTable y list <E_Sistema>
+        public DataTable GetDT_Roles()
+        {
+            return ObjLst.DT_ListadoGeneral("[tbRoles]", "NombreRol");
+        }
+
+        public List<E_Rol> LstRoles()
+        {
+            return D_ConvierteDatos.ConvertirDTALista<E_Rol>(GetDT_Roles());
+        }
+
+        // Busqueda de Roles por diferente creiterio
+
+        public E_Rol BuscarRolPorId(int pIdRol)
+        {
+            return (from User in LstRoles() where User.IdRol == pIdRol select User).FirstOrDefault();
+        }
+
+    }
+
+    public class N_Producto
+    {
+        D_IBM_Datos ObjIBM = new D_IBM_Datos();
+        D_Listados ObjLst = new D_Listados();
+        private string Sp = "IBM_Producto";
+
+        //Acciones de insertar, Borrar y Modificar los datos del Producto
+        public string InsertarProducto(E_Producto pEntidad)
+        {
+            pEntidad.Accion = "INSERTAR";
+            return ObjIBM.IBM_Entidad<E_Producto>(Sp, pEntidad);
+        }
+
+        public string BorraProducto(int pIdProducto)
+        {
+            E_Producto Entidad = new E_Producto
+            {
+                Accion = "BORRAR",
+                IdProducto = pIdProducto
+            };
+            return ObjIBM.IBM_Entidad<E_Producto>(Sp, Entidad);
+        }
+
+        public string ModoficaProducto(E_Producto pEntidad)
+        {
+            pEntidad.Accion = "MODIFICAR";
+            return ObjIBM.IBM_Entidad<E_Producto>(Sp, pEntidad);
+        }
+
+        //listado general de Producto en formato DataTable y list <E_Sistema>
+        public DataTable GetDT_Productos()
+        {
+            return ObjLst.DT_ListadoGeneral("[tbProductos]", "Marca, NombreProducto");
+        }
+
+        public List<E_Producto> LstProductos()
+        {
+            return D_ConvierteDatos.ConvertirDTALista<E_Producto>(GetDT_Productos());
+        }
+
+        // Busqueda de Productos por diferente creiterio
+
+        public E_Producto BuscarProductoPorId(int pIdProducto)
+        {
+            return (from User in LstProductos() where User.IdProducto == pIdProducto select User).FirstOrDefault();
+        }
+
+        public E_Producto BuscarProductoPorMarca(string pMarca)
+        {
+            return (from User in LstProductos() where User.Marca == pMarca select User).FirstOrDefault();
+        }
+
+        public E_Producto BuscarProductoPorNombre(String pNombreProducto)
+        {
+            return (from User in LstProductos() where User.NombreProducto == pNombreProducto select User).FirstOrDefault();
+        }
+
+
+    }
+
+    public class N_UsuarioRol
+    {
+        D_IBM_Datos ObjIBM = new D_IBM_Datos();
+        D_Listados ObjLst = new D_Listados();
+        private string Sp = "IBM_UsuarioRol";
+
+        public string InsertarUsuarioRol(E_UsuarioRol pEntidad)
+        {
+            pEntidad.Accion = "INSERTAR";
+            return ObjIBM.IBM_Entidad<E_UsuarioRol>(Sp, pEntidad);
+        }
+
+        public string BorraUsuarioRol(int pIdUsuario)
+        {
+            E_UsuarioRol Entidad = new E_UsuarioRol
+            {
+                Accion = "BORRAR",
+                IdUsuario = pIdUsuario
+            };
+            return ObjIBM.IBM_Entidad<E_UsuarioRol>(Sp, Entidad);
+        }
+
+        public string ModoficaUsuarioRol(E_UsuarioRol pEntidad)
+        {
+            pEntidad.Accion = "MODIFICAR";
+            return ObjIBM.IBM_Entidad<E_UsuarioRol>(Sp, pEntidad);
+        }
+
+        //listado general de usuario en formato DataTable y list <E_Sistema>
+        public DataTable GetDT_UsuarioRol()
+        {
+            return ObjLst.DT_ListadoGeneral("[tbUsuarioRol]", "IdUsuario");
+        }
+
+        public List<E_UsuarioRol> LstUsuarios()
+        {
+            return D_ConvierteDatos.ConvertirDTALista<E_UsuarioRol>(GetDT_UsuarioRol());
+        }
+
+        // Busqueda de usuarios por diferente creiterio
+
+        public E_UsuarioRol BuscarUsuarioRolPorId(int pIdUsuario)
+        {
+            return (from User in LstUsuarios() where User.IdUsuario == pIdUsuario select User).FirstOrDefault();
+        }
+
+        public E_UsuarioRol BuscarUsuarioRolPorRol(int pIdRol)
+        {
+            return (from User in LstUsuarios() where User.IdRol == pIdRol select User).FirstOrDefault();
+        }
+    }
+
+    public class N_UsuarioNegocios
+    {
+        D_IBM_Datos ObjIBM = new D_IBM_Datos();
+        D_Listados ObjLst = new D_Listados();
+        private string Sp = "IBM_UsuarioNegocios";
+
+        public string InsertarUsuarioNegocios(E_Negocios.E_UsuarioNegocios pEntidad)
+        {
+            pEntidad.Accion = "INSERTAR";
+            return ObjIBM.IBM_Entidad<E_Negocios.E_UsuarioNegocios>(Sp, pEntidad);
+        }
+
+        public string BorraUsuarioNegocio(int pIdUsuario)
+        {
+            E_Negocios.E_UsuarioNegocios Entidad = new E_Negocios.E_UsuarioNegocios
+            {
+                Accion = "BORRAR",
+                IdUsuario = pIdUsuario
+            };
+            return ObjIBM.IBM_Entidad<E_Negocios.E_UsuarioNegocios>(Sp, Entidad);
+        }
+
+        public string ModoficaUsuarioNegocio(E_Negocios.E_UsuarioNegocios pEntidad)
+        {
+            pEntidad.Accion = "MODIFICAR";
+            return ObjIBM.IBM_Entidad<E_Negocios.E_UsuarioNegocios>(Sp, pEntidad);
+        }
+
+        //listado general de usuario en formato DataTable y list <E_Sistema>
+        public DataTable GetDT_UsuarioNegocio()
+        {
+            return ObjLst.DT_ListadoGeneral("[tbUsuarioNegocios]", "IdUsuario");
+        }
+
+        public List<E_Negocios.E_UsuarioNegocios> LstUsuarios()
+        {
+            return D_ConvierteDatos.ConvertirDTALista<E_Negocios.E_UsuarioNegocios>(GetDT_UsuarioNegocio());
+        }
+
+        // Busqueda de usuarios por diferente creiterio
+
+        public E_Negocios.E_UsuarioNegocios BuscarUsuarioNegocioPorId(int pIdUsuario)
+        {
+            return (from User in LstUsuarios() where User.IdUsuario == pIdUsuario select User).FirstOrDefault();
+        }
+
+        public E_Negocios.E_UsuarioNegocios BuscarUsuarioNegocioPorNegocio(int pIdNegocios)
+        {
+            return (from User in LstUsuarios() where User.IdNegocios == pIdNegocios select User).FirstOrDefault();
+        }
+    }
+
+}
