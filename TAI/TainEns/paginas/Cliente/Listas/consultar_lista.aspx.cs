570cc262a3e045f7bdc1c8976c0786db05064ec6
Santiago Morales <msanty029@gmail.com>
2018-11-29 15:45:24 -0800

    Consultar Listo y eliminar Producto de lista, Falta confirmacion

--- TAI/TainEns/paginas/Cliente/Listas/consultar_lista.aspx.cs
+++ TAI/TainEns/paginas/Cliente/Listas/consultar_lista.aspx.cs
@@ -28,11 +28,15 @@ namespace TainEns.paginas.Cliente.Listas
             nombrelista.Text = (new N_ListaUsuario().BuscarListaUsuarioporLista(IdLista)).NombreLista;
             List<E_ListaProducto> LstLP = new N_ListaProducto().LstUsuarios(); 
             List < E_Producto > LstP = new List<E_Producto>();
-
-            LstP.Add((new N_Producto().BuscarProductoPorId(1)));
-            LstP.Add((new N_Producto().BuscarProductoPorId(2)));
+            foreach(E_ListaProducto LP in LstLP)
+            {
+                if(LP.IdLista == IdLista)
+                {
+                    LstP.Add(ObjNP.BuscarProductoPorId(LP.IdProducto));
+                }
+            }
             //el id producto siempre es 0, no agarra bien ese parametro
-            Label1.Text = "id producto "+LstLP[2].IdProducto+" id lista "+ LstLP[2].IdLista;
+            //Label1.Text = "id producto "+LstLP[2].IdProducto+" id lista "+ LstLP[2].IdLista;
             grvProductos.DataSource = LstP;
             grvProductos.DataBind();
         }
@@ -40,6 +44,19 @@ namespace TainEns.paginas.Cliente.Listas
         protected void grvProductos_SelectedIndexChanged(object sender, EventArgs e)
         {
             int IdProducto = Convert.ToInt16(grvProductos.SelectedDataKey["IdProducto"]);
+            int IdLista = Convert.ToInt16(Session["idlista"]);
+            List<E_ListaProducto> LstLP = new N_ListaProducto().LstUsuarios();
+            N_ListaProducto ObjNLP = new N_ListaProducto();
+
+            foreach(E_ListaProducto LP in LstLP)
+            {
+                if(LP.IdLista == IdLista && LP.IdProducto == IdProducto)
+                {
+                    string msn = ObjNLP.BorraListaProducto(LP.IdListaProducto);
+                    break;
+                }
+            }
+            grvProductos.DataBind();
         }
     }
 }
